661cf058bad3d04110eabfbab88eaf55b7a56f43
ozawaseijiro <seijiro4751@gmail.com>
2014-03-20 16:39:54 +0900

    fix

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,15 +1,9 @@
-;; emacs 23以前の場合はDefaultディレクトリを変更
+;; emacs 23以前の場合はDefaultディレクトリをっっっっｆ
 (when (< emacs-major-version 23)
   (defvar user-emacs-directory "~/.emacs.d/"))
 
 ;; 文字コード
 ;; (set-language-enviroment "Japanese")
-;; ツールバーを非表示
-;; (tool-bar-mode -1)
-;; メニューバーを非表示
-;; (menu-bar-mode -1)
-;; フォント
-;;(fixed-width-set-fontset "monaco" 14)
 
 ;; load-pathを追加
 (defun add-to-load-path (&rest paths)
@@ -22,7 +16,12 @@
 	    (normal-top-level-add-subdirs-to-load-path))))))
 
 ;; elisp public_repo confのファイルを読み込む
-(add-to-load-path "elisp" "public_repos" "conf") 
+(unless (eq window-system 'ns)
+  (menu-bar-mode 0))
+
+
+(add-to-list 'load-path "~/.emacs.d/elisp/init-loader.el")
+(add-to-load-path "elisp" "public_repos" "conf")
 (require 'init-loader)
 
 ;; 設定ファイルを読み込む
@@ -48,27 +47,36 @@
 
 (require 'package)
 
-; Add package-archives
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/")) ; ついでにmarmaladeも追加
 
-; Initialize
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t) ; ついでにmarmaladeも追加
+
+
 (package-initialize)
 
 ;;自動バックアップを中止
 (setq auto-save-default nil)
+(setq make-backup-files nil)
 
-; undotree
+(setq completion-ignore-case t)
+(global-auto-revert-mode 1)
+
+					;undotree
+(el-get 'sync 'undo-tree)
 (require 'undo-tree)
 (define-key global-map (kbd "C-x C-u") 'undo-tree-visualize)
 
-; maggit
+					; maggit
+(el-get 'sync 'magit)
 (require 'magit)
 
-; undohist
+					; undohist
+(el-get 'sync 'undohist)
 (require 'undohist)
+(undohist-initialize)
 
 ;; php-mode
+(el-get 'sync 'php-mode)
 (require 'php-mode)
 
 ;; helm
@@ -78,48 +86,51 @@
 
 (when (require 'helm-descbinds)
   ;; describe-bindings をhelmに変更
-  (helm-descbinds-mode t))
-  
-(define-key global-map (kbd "C-x f") 'helm-for-files)
-(define-key global-map (kbd "C-x l") 'helm-find-files)
-(define-key global-map (kbd "M-y") 'helm-show-kill-ring) 
-(define-key global-map (kbd "M-x") 'helm-M-x)
-(define-key global-map (kbd "C-x C-r") 'helm-recentf)
+  (helm-descbinds-mode t)
+  (define-key global-map (kbd "C-x f") 'helm-for-files)
+  (define-key global-map (kbd "C-x l") 'helm-find-files)
+  (define-key global-map (kbd "M-y") 'helm-show-kill-ring) 
+  (define-key global-map (kbd "M-x") 'helm-M-x)
+  (define-key global-map (kbd "C-x C-r") 'helm-recentf))
 
 ;;; popwin.el
+(el-get 'sync 'popwin)
 (when (require 'popwin)
   (setq helm-samewindow nil)
   (setq display-buffer-function 'popwin:display-buffer)
   (setq popwin:special-display-config '(("*compilation*" :noselect t)
 					("helcm" :regexp t :height 0.4)
 					)))
-;; 
+
 ;; auto-complete
+(el-get 'sync 'auto-complete)
 (require 'auto-complete)
 (require 'auto-complete-config)
 (global-auto-complete-mode t)
 
-(define-key ac-completing-map (kbd "C-s") 'ac-next) ; M-nで次候補選択
-(define-key ac-completing-map (kbd "C-r") 'ac-previous)  ; M-pで前候補選択
+(define-key ac-completing-map (kbd "C-s") 'ac-next) ; C-sで次候補選択
+(define-key ac-completing-map (kbd "C-r") 'ac-previous)  ; C-rで前候補選択
 (ac-set-trigger-key "TAB")  ; TABで補完開始(トリガーキー)
-  
+
 (put 'set-goal-column 'disabled nil)
 
- ;; color-moccur
-;; (when (require 'color-moccur)
-;;   (define-key global-map (kbd "M-o") 'occur-by-moccur)
-;;   ;スペースで区切られた複数の単語にマッチさせる
-;;   (setq moccur-split-word t)
-;;   ;;ディレクトリ検索の際に除外
-;;   (add-to-list 'dmoccur-exclusion-mask "\\.DS_Store")
-;;   (add-to-list 'dmoccur-exclusion-mask "^#.+#$"))
+;; color-moccur
+(el-get 'sync 'color-moccur)
+(when (require 'color-moccur)
+  (define-key global-map (kbd "M-o") 'occur-by-moccur)
+					;スペースで区切られた複数の単語にマッチさせる
+  (setq moccur-split-word t)
+  ;;ディレクトリ検索の際に除外
+  (add-to-list 'dmoccur-exclusion-mask "\\.DS_Store")
+  (add-to-list 'dmoccur-exclusion-mask "^#.+#$"))
 
-;; helm-c-moccur
+;; helm-c-moccu
 (when (require 'helm-c-moccur)
   (global-set-key (kbd "M-o") 'helm-c-moccur-occur-by-moccur)
   (global-set-key (kbd "C-M-o") 'helm-c-moccur-dmoccur)
-  (global-set-key (kbd "C-s") 'helm-c-moccur-isearch-forward)
-  (global-set-key (kbd "C-r") 'helm-c-moccur-isearch-backward))
+  (global-set-key (kbd "C-M-s") 'helm-c-moccur-isearch-forward)
+  (global-set-key (kbd "C-M-r
+") 'helm-c-moccur-isearch-backward))
 
 ;; python-mode
 ;;(require 'pyton-mode)
@@ -127,6 +138,34 @@
 ;; git-gutter
 (when (require 'git-gutter+)
   (global-git-gutter+-mode t))
-  
+
+;; theme
+
+;;(load-theme 'sanityinc-solarized-dark t)
+
+;; paredit
+;; http://www.daregada.sakuraweb.com/paredit_tutorial_ja.html
+;(el-get 'sync 'paredit)
+;(add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+
+;; (require 'wgrep nil t)
+
+;; (require 'jinja2-mode nil t)
+
+;; 現在行をハイライト
+(global-hl-line-mode 0)
 
 
+;;; ファイル内のカーソル位置を保存する
+(setq-default save-place t)
+
+;;; 対応する括弧を表示させる
+(show-paren-mode 1)
+(require 'scala-mode2)
+(require 'coffee-mode)
+(require 'flymake-coffee)
+(require 'js3-mode)
+(require 'cl)
+(when (require 'helm-project)
+  (global-set-key (kbd "C-M-p") 'helm-project)
+  (global-set-key (kbd "M-t") 'helm-project-grep))
