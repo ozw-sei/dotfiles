22fa535ed47efb78a33ff3bad31285662847f585
ozawaseijiro <seijiro4751@gmail.com>
2014-06-09 15:10:33 +0900

    delete needn't files

--- emacs.d/conf/020_ctags.el
+++
@@ -1,4 +0,0 @@
-(require 'ctags nil t)
-(setq tags-revert-without-query t)
-;;(setq ctags-command "ctags -R --fields=\"+afikKlmnsSzt\" ")
-(global-set-key (kbd "<f5>") 'ctags-create-or-update-tags-table)
--- emacs.d/conf/999_keybind.el
+++
@@ -1,5 +0,0 @@
-;; C-mをインデント付き改行に変更
-(define-key global-map (kbd "C-m") 'newline-and-indent)
-
-;;C-hをバックスペースにする
-(keyboard-translate ?\C-h ?\C-?)
