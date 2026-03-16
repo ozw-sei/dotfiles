af8c4339503655df65631d3b329d9f02ea247296
ozawaseijiro <seijiro4751@gmail.com>
2026-03-16 10:29:29 +0900

    fix

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -2,7 +2,8 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'Solarized Light (Gogh)'
-config.send_composed_key_when_left_alt_is_pressed = false                     config.send_composed_key_when_right_alt_is_pressed = false
+config.send_composed_key_when_left_alt_is_pressed = false
+config.send_composed_key_when_right_alt_is_pressed = false
 
 
 return config
